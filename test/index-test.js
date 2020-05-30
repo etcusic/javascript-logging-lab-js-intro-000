1860934af11ac79e8e27843f4e2379aa3b64f924
Eli Cusic <tntitans31@gmail.com>
2020-05-30 01:09:54 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -1,3 +1,5 @@
+console.error("HALP!")
+
 const expect = require('expect')
 const fs = require('fs')
 const jsdom = require('jsdom')
@@ -44,4 +46,3 @@ describe('index', () => {
     })
   })
 })
-console.error("HALP!")
